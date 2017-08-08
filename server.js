2384bfa93a0ca2a869d89fef8fcefe35da912a50
vigneshu <--global>
2017-08-08 00:02:40 -0400

    server.js updated

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 var app = require('./express');
 var express = app.express;
 var bodyParser = require('body-parser');
-
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
-
-require("./test/app");
 require("./assignment/app");
+require("./project/app");
 var port = process.env.PORT || 3000;
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port_cors =  8080;
+var proxyUrl = "http://localhost:8080/";
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    proxyUrl = process.env.HOST + "/";
+}
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port_cors, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port_cors);
+});
 app.listen(port);
