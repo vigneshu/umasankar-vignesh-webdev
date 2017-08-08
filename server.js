53bf31e8108e129e9a1550e685a09673883eb36f
vigneshu <--global>
2017-08-08 01:10:28 -0400

    cors

--- server.js
+++ server.js
@@ -9,18 +9,28 @@ require("./assignment/app");
 require("./project/app");
 var port = process.env.PORT || 3000;
 
-var host = process.env.HOST ? '0.0.0.0' : '127.0.0.1';
+var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port_cors =  8080;
-var proxyUrl = "http://localhost:8080/";
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    proxyUrl = process.env.HOST + "/";
-}
+
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    removeHeaders: [
+        'cookie',
+        'cookie2',
+        // Strip Heroku-specific headers
+        'x-heroku-queue-wait-time',
+        'x-heroku-queue-depth',
+        'x-heroku-dynos-in-use',
+        'x-request-start',
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+        // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+        xfwd: false,
+    },
 }).listen(port_cors, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port_cors);
 });
